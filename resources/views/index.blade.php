cff029675bab3adf3aa7fec39f4ce272bf5f4644
ryanlemay0607 <lemay.ryan@gmail.com>
2017-07-27 12:26:47 -0400

    Update fav icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="ProteusThemes">
-    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="shortcut icon" href="images/fav.jpeg">
     <title>Weekly Picks</title>
 
     <!-- Custom styles for this template -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="images/fav.jpeg">
+    <link rel="stylesheet" href="/stylesheets/3eba4af4.main.css"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
