b2700de2aa36e6b4503b8d6409fa433f8e52d588
ryanlemay0607 <lemay.ryan@gmail.com>
2017-10-24 14:48:08 -0400

    Added Weekly Winner

---
+++ database/migrations/2017_10_24_184135_change_paid_to.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePaidTo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->integer('total_paid')->default('0')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
