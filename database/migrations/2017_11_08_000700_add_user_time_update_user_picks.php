7f4e31a92ab7919725e8e26273cb67e08f3b840c
ryanlemay0607 <lemay.ryan@gmail.com>
2017-11-07 19:11:26 -0500

    Added Weekly Winner

---
+++ database/migrations/2017_11_08_000700_add_user_time_update_user_picks.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserTimeUpdateUserPicks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_picks', function($table)
+        {
+            $table->dateTime('user_update_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
